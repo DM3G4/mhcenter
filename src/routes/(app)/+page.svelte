a42b69be8f86146e39251dd38fc955ae7aba339b
DM3G4 <diegom3g4@gmail.com>
2025-02-16 19:50:41 -0600

    Initial commit

---
+++ src/routes/(app)/+page.svelte
@@ -0,0 +1,63 @@
+<h1>Insect Glaive Combos</h1>
+<div class="grid">
+  <article>
+    <header>Rising Slash Combo</header>
+    <img src="keyboard\mouse_left.svg" alt="MBL" />
+  </article>
+
+  <article>
+    <header>Wide Sweep</header>
+    <img src="keyboard\mouse_right.svg" alt="MBR" />
+  </article>
+
+  <article>
+    <header>Kinsect: Mark Target</header>
+    <img src="keyboard\keyboard_r_outline.svg" alt="R" />
+  </article>
+</div>
+
+<div class="grid">
+  <article>
+    <header>Kinsect: Harvest Extract</header>
+    <img src="keyboard\keyboard_r_outline.svg" alt="R" /> +
+    <img src="keyboard\mouse_left.svg" alt="MBL" />
+  </article>
+
+  <article>
+    <header>Kinsect: Fire</header>
+    <img src="keyboard\keyboard_r_outline.svg" alt="R" /> +
+    <img src="keyboard\keyboard_shift_outline.svg" alt="Shift" />
+  </article>
+
+  <article>
+    <header>Kinsect: Recall <small>(While Kinsect is in Flight)</small></header>
+    <img src="keyboard\keyboard_r_outline.svg" alt="R" /> +
+    <img src="keyboard\mouse_right.svg" alt="MBR" />
+  </article>
+</div>
+
+<div class="grid">
+  <article>
+    <header>Rising Spiral Slash <small>(Requires all extracts)</small></header>
+    <img src="keyboard\mouse_right.svg" alt="MBR" /> (Hold & Release) &nbsp; >
+    <img src="keyboard\mouse.svg" alt="Mouse" />
+    <img src="keyboard\keyboard_2_outline.svg" alt="MB2" />
+    + <img src="keyboard\mouse_right.svg" alt="MBR" />
+  </article>
+
+  <article>
+    <header>Focus Thrust: Leaping Strike <small>(Extra damage to wound) (Aerial & Ground)</small></header>
+    <img src="keyboard\mouse.svg" alt="Mouse" />
+    <img src="keyboard\keyboard_2_outline.svg" alt="MB2" />
+    + <img src="keyboard\keyboard_shift_outline.svg" alt="Shift" />
+  </article>
+</div>
+
+<article>
+  <header>Vault</header>
+  <img src="keyboard\keyboard_r_outline.svg" alt="R" /> +
+  <img src="keyboard\keyboard_space_outline.svg" alt="Space" /> >
+  <img src="keyboard\mouse_right.svg" alt="MBR" /> or
+  <img src="keyboard\mouse_left.svg" alt="MBL" /> or
+  <img src="keyboard\keyboard_space_outline.svg" alt="Space" />
+</article>
---
+++ src/routes/+layout.svelte
@@ -0,0 +1,15 @@
+<script>
+  import "@picocss/pico/css/pico.css"
+
+  let { children } = $props()
+</script>
+
+<main class="container">
+  {@render children()}
+</main>
+
+<style>
+  main {
+    margin-top: 24px;
+  }
+</style>
